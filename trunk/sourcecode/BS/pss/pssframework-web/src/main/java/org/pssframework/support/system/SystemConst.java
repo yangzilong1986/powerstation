788f4b198949b4ec073726d9e4e5654bd2b18853
59471032@qq.com <59471032@qq.com@2f2c2142-db03-11de-849f-b362a63f070a>
2010-07-11 07:24:10 +0000

    Failed commit: Default

--- trunk/sourcecode/BS/pss/pssframework-web/src/main/java/org/pssframework/support/system/SystemConst.java
+++ trunk/sourcecode/BS/pss/pssframework-web/src/main/java/org/pssframework/support/system/SystemConst.java
@@ -1,199 +1,207 @@
 /**
- * 
+ *
  */
 package org.pssframework.support.system;
 
 /**
  * @author Administrator
- *
  */
 public final class SystemConst {
 
-	/*********************************SYSTEM********************************************/
-	// 大用户
-	public final static String SYSTEM_BIG_CUSTOMER = "1";
+    /**
+     * ******************************SYSTEM*******************************************
+     */
+    // 大用户
+    public final static String SYSTEM_BIG_CUSTOMER = "1";
 
-	// 台区
-	public final static String SYSTEM_TG = "2";
+    // 台区
+    public final static String SYSTEM_TG = "2";
 
-	// 变电站
-	public final static String SYSTEM_SUBSTATION = "3";
+    // 变电站
+    public final static String SYSTEM_SUBSTATION = "3";
 
-	// 低压集抄
-	public final static String SYSTEM_LOW_PRESSURE = "4";
+    // 低压集抄
+    public final static String SYSTEM_LOW_PRESSURE = "4";
 
-	/*********************************MESSAGE********************************************/
+    /**
+     * ******************************MESSAGE*******************************************
+     */
 
-	public final static String MSG_CREATED_SUCCESS = "创建成功";
+    public final static String MSG_CREATED_SUCCESS = "创建成功";
 
-	public final static String MSG_UPDATE_SUCCESS = "更新成功";
+    public final static String MSG_UPDATE_SUCCESS = "更新成功";
 
-	public final static String MSG_DELETE_SUCCESS = "删除成功";
+    public final static String MSG_DELETE_SUCCESS = "删除成功";
 
-	public final static String MSG_CREATED_FAIL = "创建失败";
+    public final static String MSG_CREATED_FAIL = "创建失败";
 
-	public final static String MSG_UPDATE_FAIL = "更新失败";
+    public final static String MSG_UPDATE_FAIL = "更新失败";
 
-	public final static String MSG_DELETE_FAIL = "删除失败";
+    public final static String MSG_DELETE_FAIL = "删除失败";
 
-	/*********************************CONTROLLER******************************************/
-	/**
-	 * 方法
-	 */
-	public static final String CONTROLLER_METHOD_TYPE = "_type";
+    /*********************************CONTROLLER******************************************/
+    /**
+     * 方法
+     */
+    public static final String CONTROLLER_METHOD_TYPE = "_type";
 
-	public static final String CONTROLLER_METHOD_TYPE_NEW = "new";
+    public static final String CONTROLLER_METHOD_TYPE_NEW = "new";
 
-	public static final String CONTROLLER_METHOD_TYPE_DELETE = "delete";
+    public static final String CONTROLLER_METHOD_TYPE_DELETE = "delete";
 
-	public static final String CONTROLLER_METHOD_TYPE_EDIT = "edit";
+    public static final String CONTROLLER_METHOD_TYPE_EDIT = "edit";
 
-	public static final String CONTROLLER_METHOD_TYPE_SHOW = "show";
+    public static final String CONTROLLER_METHOD_TYPE_SHOW = "show";
 
-	public static final String CONTROLLER_METHOD_TYPE_UPDATE = "update";
+    public static final String CONTROLLER_METHOD_TYPE_UPDATE = "update";
 
-	public static final String CONTROLLER_METHOD_TYPE_INDEX = "index";
+    public static final String CONTROLLER_METHOD_TYPE_INDEX = "index";
 
-	/**
-	 * ajax是否成功
-	 */
-	public static final String CONTROLLER_AJAX_IS_SUCC = "cAjaxIsSucc";
+    /**
+     * ajax是否成功
+     */
+    public static final String CONTROLLER_AJAX_IS_SUCC = "cAjaxIsSucc";
 
-	/**
-	 * ajax消息
-	 */
-	public static final String CONTROLLER_AJAX_MESSAGE = "cAjaxMsg";
+    /**
+     * ajax消息
+     */
+    public static final String CONTROLLER_AJAX_MESSAGE = "cAjaxMsg";
 
-	/*********************************ACODE***********************************************/
-	/**
-	 * CT
-	 */
-	public static final String CODE_CT_RATIO = "CT_RATIO";
+    /*********************************ACODE***********************************************/
+    /**
+     * CT
+     */
+    public static final String CODE_CT_RATIO = "CT_RATIO";
 
-	/**
-	 * PT
-	 */
-	public static final String CODE_PT_RATIO = "PT_RATIO";
+    /**
+     * PT
+     */
+    public static final String CODE_PT_RATIO = "PT_RATIO";
 
-	/**
-	 *通讯方式
-	 */
-	public static final String CODE_COMM_MODE = "COMM_MODE";
+    /**
+     * 终端通讯方式
+     */
+    public static final String CODE_COMM_MODE = "COMM_MODE";
 
-	/**
-	 *波特率式
-	 */
-	public static final String CODE_BTL = "BTL";
+    /**
+     * 电表通讯方式
+     */
+    public static final String CODE_COMM_MODE_GM = "COMM_MODE_GM";
 
-	/**
-	 *接线方式
-	 */
-	public static final String CODE_WIRING_MODE = "WIRING_MODE";
+    /**
+     * 波特率式
+     */
+    public static final String CODE_BTL = "BTL";
 
-	/**
-	 *计量方式
-	 */
-	public static final String CODE_MEAS_MODE = "MEAS_MODE";
+    /**
+     * 接线方式
+     */
+    public static final String CODE_WIRING_MODE = "WIRING_MODE";
 
-	/**
-	 *表 规 约
-	 */
-	public static final String CODE_PROTOCOL_METER = "PROTOCOL_METER";
+    /**
+     * 计量方式
+     */
+    public static final String CODE_MEAS_MODE = "MEAS_MODE";
 
-	/**
-	 *台区状态
-	 */
-	public static final String CODE_TG_STATUS = "TG_STATUS";
+    /**
+     * 表 规 约
+     */
+    public static final String CODE_PROTOCOL_METER = "PROTOCOL_METER";
 
-	/**
-	 * 变压器型号
-	 */
-	public static final String CODE_TRAN_CODE = "TRAN_CODE";
+    /**
+     * 台区状态
+     */
+    public static final String CODE_TG_STATUS = "TG_STATUS";
 
-	/**
-	 * 变压器状态
-	 */
-	public static final String CODE_TRAN_STATUS = "TRAN_STATUS";
+    /**
+     * 变压器型号
+     */
+    public static final String CODE_TRAN_CODE = "TRAN_CODE";
 
-	/**
-	 * 电压等级
-	 */
-	public static final String CODE_VOLT_GRADE = "VOLT_GRADE";
+    /**
+     * 变压器状态
+     */
+    public static final String CODE_TRAN_STATUS = "TRAN_STATUS";
 
-	/**
-	 * 额定电流
-	 */
-	public static final String CODE_RATED_EC = "RATED_EC";
+    /**
+     * 电压等级
+     */
+    public static final String CODE_VOLT_GRADE = "VOLT_GRADE";
 
-	/**
-	 * REMC_GEAR 剩余电流档位
-	 */
-	public static final String CODE_REMC_GEAR = "REM_EC";
+    /**
+     * 额定电流
+     */
+    public static final String CODE_RATED_EC = "RATED_EC";
 
-	/**
-	 * 剩余电流档位当前值 REMC_GEAR_VALUE
-	 */
-	public static final String CODE_REMC_GEAR_VALUE = "REM_EC";
+    /**
+     * REMC_GEAR 剩余电流档位
+     */
+    public static final String CODE_REMC_GEAR = "REM_EC";
 
-	/**
-	 * -漏电分断延迟档位 OFF_DELAY_GEAR
-	 */
-	public static final String CODE_OFF_DELAY_GEAR = "OFF_DELAY_VALUE";
+    /**
+     * 剩余电流档位当前值 REMC_GEAR_VALUE
+     */
+    public static final String CODE_REMC_GEAR_VALUE = "REM_EC";
 
-	/**
-	 * 漏电分断延迟时间 OFF_DELAY_VALUE
-	 */
-	public static final String CODE_OFF_DELAY_VALUE = "OFF_DELAY_VALUE";
+    /**
+     * -漏电分断延迟档位 OFF_DELAY_GEAR
+     */
+    public static final String CODE_OFF_DELAY_GEAR = "OFF_DELAY_VALUE";
 
-	/**
-	 * 漏保类型
-	 */
-	public static final String CODE_PS_TYPE = "PS_TYPE";
+    /**
+     * 漏电分断延迟时间 OFF_DELAY_VALUE
+     */
+    public static final String CODE_OFF_DELAY_VALUE = "OFF_DELAY_VALUE";
 
-	/**
-	 * 漏保型号
-	 */
-	public static final String CODE_PS_MODEL = "PS_MODEL";
+    /**
+     * 漏保类型
+     */
+    public static final String CODE_PS_TYPE = "PS_TYPE";
 
-	/**
-	 * 漏保规约
-	 */
-	public static final String CODE_PROTOCOL_PS = "PROTOCOL_PS";
+    /**
+     * 漏保型号
+     */
+    public static final String CODE_PS_MODEL = "PS_MODEL";
 
-	/**
-	 * 终端规约
-	 */
-	public static final String CODE_PROTOCOL_TERM = "PROTOCOL_TERM";
+    /**
+     * 漏保规约
+     */
+    public static final String CODE_PROTOCOL_PS = "PROTOCOL_PS";
 
-	/**
-	 * 当前状态
-	 */
-	public static final String CODE_CUR_STATUS = "CUR_STATUS";
+    /**
+     * 终端规约
+     */
+    public static final String CODE_PROTOCOL_TERM = "PROTOCOL_TERM";
 
-	/**
-	 * 终端类型
-	 */
-	public static final String CODE_TERM_TYPE = "TERM_TYPE";
+    /**
+     * 当前状态
+     */
+    public static final String CODE_CUR_STATUS = "CUR_STATUS";
 
-	/**
-	 * 厂家
-	 */
-	public static final String CODE_MADE_FAC = "MADE_FAC";
+    /**
+     * 终端类型
+     */
+    public static final String CODE_TERM_TYPE = "TERM_TYPE";
 
-	/**
-	 * 产权
-	 */
-	public static final String CODE_PR = "PR";
+    /**
+     * 厂家
+     */
+    public static final String CODE_MADE_FAC = "MADE_FAC";
 
-	/**
-	 * 电表状态
-	 */
-	public static final String CODE_METER_STATUS = "METER_STATUS";
+    /**
+     * 产权
+     */
+    public static final String CODE_PR = "PR";
 
-	/**
-	 * 运行状态
-	 */
-	public static final String CODE_RUN_STATUS = "RUN_STATUS";
+    /**
+     * 电表状态
+     */
+    public static final String CODE_METER_STATUS = "METER_STATUS";
 
-	/***********************************************************************************/
+    /**
+     * 运行状态
+     */
+    public static final String CODE_RUN_STATUS = "RUN_STATUS";
+
+    /***********************************************************************************/
 }
