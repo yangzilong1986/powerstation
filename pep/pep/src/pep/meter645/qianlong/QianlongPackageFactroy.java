b46fd3e9b5b7a657e3680872f444ea03ebd75b33
luxiaochunG <luxiaochunG@2f2c2142-db03-11de-849f-b362a63f070a>
2010-08-01 06:49:20 +0000

    增加漏保的命令生成器。

---
+++ pep/pep/src/pep/meter645/qianlong/QianlongPackageFactroy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pep.meter645.qianlong;
+
+import pep.meter645.Gb645MeterPacket;
+
+/**
+ *
+ * @author luxiaochung
+ */
+public class QianlongPackageFactroy {
+    public static Gb645MeterPacket makeReadPacket(String address, int dataId){
+        Gb645MeterPacket pack = new Gb645MeterPacket(address);
+        pack.getControlCode().isFromMast(true).isHasHouxuzhen(false).isYichang(false).setFuncCode((byte)1);
+        pack.getData().putWord(dataId);
+        return pack;
+    }
+}
