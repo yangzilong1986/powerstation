8832a558350433fd7d203546b7aa06f3cd9d02bc
luxiaochunG <luxiaochunG@2f2c2142-db03-11de-849f-b362a63f070a>
2010-08-01 06:47:41 +0000

    修改ERC36的定义，格式已经修改。

--- pep/pep/src/pep/codec/protocol/gb/gb376/Packet376Event36.java
+++ pep/pep/src/pep/codec/protocol/gb/gb376/Packet376Event36.java
@@ -2,10 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pep.codec.protocol.gb.gb376;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import pep.codec.utils.BcdDataBuffer;
 import pep.meter645.Gb645Address;
@@ -15,42 +15,124 @@ import pep.meter645.Gb645Address;
  * @author luxiaochung
  */
 public class Packet376Event36 extends PmPacket376EventBase {
-    public class Meter{
+
+    public class Meter {
+
         public String meterAddress;
-        public byte xiangweiHeTongxunPinzhi;
-        public byte meterProtocol;
+        public boolean isClosed;
+        public boolean isLocked;
+        public String xiangwei;
+        public byte status;
+        public Date eventTime;
+        public int actValue;
+
+        public String statusString() {
+            switch (this.status) {
+                case 0:
+                    return "漏电跳闸";
+                case 1:
+                    return "突变跳闸";
+                case 2:
+                    return "特波跳闸";
+                case 3:
+                    return "过载跳闸";
+                case 4:
+                    return "过压跳闸";
+                case 5:
+                    return "欠压跳闸";
+                case 6:
+                    return "短路跳闸";
+                case 7:
+                    return "手动跳闸";
+                case 8:
+                    return "停电跳闸";
+                case 9:
+                    return "互感器故障跳闸";
+                case 10:
+                    return "远程跳闸";
+                case 11:
+                    return "其它原因跳闸";
+                case 12:
+                    return "合闸过程中";
+                case 13:
+                    return "合闸失败";
+                default:
+                    return "未知";
+            }
+        }
 
         @Override
-        public String toString(){
+        public String toString() {
             StringBuilder sb = new StringBuilder();
             sb.append("meter address=").append(meterAddress).append(" ");
-            sb.append("protocol=").append(meterProtocol);
+            if (this.isClosed) {
+                sb.append("合闸状态 ");
+            } else {
+                sb.append("分闸状态 ");
+            }
+            if (this.isLocked) {
+                sb.append("锁死状态 ");
+            } else {
+                sb.append("未锁死状态 ");
+            }
+            sb.append("相位").append(xiangwei).append(" ");
+            sb.append(this.statusString());
             return sb.toString();
         }
     }
-
+    
     public byte Tongxunduankou;
     public List<Meter> meters = new ArrayList<Meter>();
 
     @Override
     protected void DecodeEventDetail(BcdDataBuffer eventData, int len) {
-        if (eventData.restBytes()<2) return;
+        if (eventData.restBytes() < 2) {
+            return;
+        }
         this.Tongxunduankou = (byte) (eventData.getByte() & 0x3F);
         len -= 2;
 
-        while ((eventData.restBytes()>=8) && (len>0)){
-            Meter event = new Meter();
-            event.meterAddress = Gb645Address.meterAddressToString(eventData.getBytes(6));
-            event.xiangweiHeTongxunPinzhi = (byte) eventData.getByte();
-            event.meterProtocol = (byte) eventData.getByte();
-            meters.add(event);
-            len -= 8;
+        while ((eventData.restBytes() >= 8) && (len > 0)) {
+            try {
+                Meter event = new Meter();
+                event.meterAddress = Gb645Address.meterAddressToString(eventData.getBytes(6));
+                byte s = (byte) eventData.getByte();
+                event.isClosed = ((s & 0x80) == 0x80);
+                event.isLocked = ((s & 0x40) == 0x40);
+                switch ((s & 0x30) >> 4) {
+                    case 0:
+                        event.xiangwei = "无效";
+                        break;
+                    case 1:
+                        event.xiangwei = "A相";
+                        break;
+                    case 2:
+                        event.xiangwei = "B相";
+                        break;
+                    case 3:
+                        event.xiangwei = "C相";
+                        break;
+                }
+                event.status = (byte) (s & 0x0f);
+
+                try {
+                    event.actValue = (int) eventData.getBcdInt(2);
+                } catch (Exception ex) {
+                    event.actValue = 0;
+                }
+
+                event.eventTime = eventData.getDate("MIHHDDMM");
+
+                meters.add(event);
+            } finally {
+                len -= 8;
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         sb.append("Comm port=").append(this.Tongxunduankou).append(" ");
         sb.append("meters=(");
-        for(Meter event: this.meters){
+        for (Meter event : this.meters) {
             sb.append("(").append(event.toString()).append(")");
         }
         sb.append(")");
