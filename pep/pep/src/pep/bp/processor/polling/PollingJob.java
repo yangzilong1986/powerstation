e3c014b0208305ba18cba89207947c296425a9b3
xiekeli <xiekeli@gmail.com>
2010-08-12 12:20:26 +0000

    收到下发报文强制下发未发报文。

--- pep/pep/src/pep/bp/processor/polling/PollingJob.java
+++ pep/pep/src/pep/bp/processor/polling/PollingJob.java
@@ -1,8 +1,8 @@
 /*
  * 主站轮召处理器
  */
-
 package pep.bp.processor.polling;
+
 import java.util.List;
 
 import org.quartz.Job;
@@ -32,6 +32,7 @@ import pep.system.SystemConst;
  * @author Thinkpad
  */
 public class PollingJob implements Job {
+
     private final static Logger log = LoggerFactory.getLogger(PollingJob.class);
     private TaskService taskService;
     private PepCommunicatorInterface pepCommunicator;//通信代理器
@@ -39,17 +40,16 @@ public class PollingJob implements Job {
     private ApplicationContext cxt;
     private Converter converter;
     private int circleUnit;
-    private int sequenceCode=0;
+    private int sequenceCode = 0;
 
-
-    private int getsequenceCode(){
+    private int getsequenceCode() {
         return sequenceCode++;
     }
 
-    public  PollingJob(PepCommunicatorInterface pepCommunicator,int circleUnit){
+    public PollingJob(PepCommunicatorInterface pepCommunicator, int circleUnit) {
         cxt = new ClassPathXmlApplicationContext(SystemConst.SPRING_BEANS);
         taskService = (TaskService) cxt.getBean("taskService");
-        converter = (Converter)cxt.getBean("converter");
+        converter = (Converter) cxt.getBean("converter");
         this.pepCommunicator = pepCommunicator;
         this.circleUnit = circleUnit;
     }
@@ -57,14 +57,16 @@ public class PollingJob implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         List<TermTaskDAO> TermTaskList = taskService.getPollingTask(circleUnit);
-        for(TermTaskDAO task:TermTaskList){
-            DoTask(this.pepCommunicator,task);
+        if (null != TermTaskList) {
+            for (TermTaskDAO task : TermTaskList) {
+                DoTask(this.pepCommunicator, task);
+            }
         }
     }
 
-    private void DoTask(PepCommunicatorInterface pepCommunicator,TermTaskDAO task){
+    private void DoTask(PepCommunicatorInterface pepCommunicator, TermTaskDAO task) {
         List<CommanddItemDAO> CommandItemList = task.getCommandItemList();
-        for(CommanddItemDAO commandItemDao:CommandItemList){
+        for (CommanddItemDAO commandItemDao : CommandItemList) {
             CollectObject object = new CollectObject();
             CommandItem Item = new CommandItem();
             Item.setIdentifier(commandItemDao.getCommandItemCode());
@@ -73,10 +75,10 @@ public class PollingJob implements Job {
             object.setLogicalAddr(task.getLogicAddress());
             object.setMpSn(new int[]{task.getGp_sn()});
             PmPacket376 packet = new PmPacket376();
-            packet.getAddress().setMastStationId((byte)2);
-            converter.CollectObject2Packet(object, packet,task.getAFN(),new StringBuffer(), new StringBuffer());
+            packet.getAddress().setMastStationId((byte) 2);
+            converter.CollectObject2Packet(object, packet, task.getAFN(), new StringBuffer(), new StringBuffer());
             pepCommunicator.SendPacket(this.getsequenceCode(), packet);
-            log.info("下发轮召报文（命令项;"+Item.getIdentifier()+"）："+BcdUtils.binArrayToString(packet.getValue()) );
-        }        
+            log.info("下发轮召报文（命令项;" + Item.getIdentifier() + "）：" + BcdUtils.binArrayToString(packet.getValue()));
+        }
     }
 }
