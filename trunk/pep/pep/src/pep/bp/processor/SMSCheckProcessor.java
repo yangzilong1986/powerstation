6ac1d3a9e4a6ef836ac8b5a1068e42df3ddc2818
luxiaochunG <luxiaochunG@2f2c2142-db03-11de-849f-b362a63f070a>
2010-08-21 09:38:20 +0000

    更改类名。

---
+++ trunk/pep/pep/src/pep/bp/processor/SMSCheckProcessor.java
@@ -0,0 +1,16 @@
+/*
+ * 短信通知处理器
+ */
+
+package pep.bp.processor;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class SMSCheckProcessor implements Runnable{
+    @Override
+    public void run(){
+        
+    }
+}
