6f5929b1be414c1f17e3b27e8477d3cd41fc6cd8
luxiaochunG <luxiaochunG@2f2c2142-db03-11de-849f-b362a63f070a>
2010-08-21 09:37:28 +0000

    增加短消息发起的合闸操作返回处理。

--- trunk/pep/pep/src/pep/bp/processor/ResponseDealer.java
+++ trunk/pep/pep/src/pep/bp/processor/ResponseDealer.java
@@ -12,6 +12,7 @@ import pep.mina.common.RtuAutoUploadPacketQueue;
 import pep.mina.common.RtuRespPacketQueue;
 import pep.mina.common.SequencedPmPacket;
 import pep.mina.common.SequencedPmPacket.Status;
+import pep.mina.protocolcodec.gb.RtuCommunicationInfo;
 import pep.system.SystemConst;
 
 /**
@@ -19,7 +20,6 @@ import pep.system.SystemConst;
  * @author Thinkpad
  */
 public class ResponseDealer extends BaseProcessor {
-
     private final static Logger log = LoggerFactory.getLogger(ResponseDealer.class);
     private RTTaskService taskService;
     private PepCommunicatorInterface pepCommunicator;//通信代理器
@@ -42,9 +42,12 @@ public class ResponseDealer extends BaseProcessor {
                 SequencedPmPacket packet = respQueue.PollPacket();
 
                 if ((packet.status == Status.SUSSESS) || (packet.status == Status.TO_BE_CONTINUE)) {
-                    if (packet.pack.getAddress().getMastStationId() == 2)//主动轮召任务返回处理
+                    if (packet.pack.getAddress().getMastStationId() == RtuCommunicationInfo.AUTO_CALL_TASK_HOSTID)//主动轮召任务返回处理
                     {
                         upLoadQueue.addPacket(packet.pack);
+                    } else if (packet.pack.getAddress().getMastStationId() == RtuCommunicationInfo.LOUBAO_OPRATE_HOSTID)//漏保恢复尝试
+                    {
+                        SmsRespProcessor.receiveRtuPacket(packet.pack);
                     } else //实时召测任务返回处理
                     {
                         taskService.insertRecvMsg(packet.sequence, packet.pack.getAddress().getRtua(), BcdUtils.binArrayToString(packet.pack.getValue()));
