53b064e013dbb25b18b88f596b67b1c9b5d9aff9
xiekeli@gmail.com <xiekeli@gmail.com@2f2c2142-db03-11de-849f-b362a63f070a>
2012-03-28 13:33:23 +0000

    增加04事件处理

--- trunk/pep2.0/pep/src/pep/codec/protocol/gb/gb376/Packet376EventNormal.java
+++ trunk/pep2.0/pep/src/pep/codec/protocol/gb/gb376/Packet376EventNormal.java
@@ -16,6 +16,17 @@ public class Packet376EventNormal extends PmPacket376EventBase{
 
     @Override
     protected void DecodeEventDetail(BcdDataBuffer eventData, int len) {
-        this.eventDetail = BcdUtils.binArrayToString(eventData.getBytes(len));
+        switch(erc)
+        {
+            case 4:{  //开关量状态变位
+                this.eventDetail = BcdUtils.bytesToBitSetString(eventData.getBytes(len));
+                break;
+            }
+            case 14:{//停上电事件
+                this.eventDetail = BcdUtils.binArrayToString(eventData.getBytes(len));
+                break;
+            }
+        }
+        
     }
 }
